f7a314a66a42ae06938592a0a49cd05fb8d44e97
unknown <prickprat>
2015-08-13 18:54:16 +1000

    creating a mock tester for sdn api

---
+++ sdnXmlParser.py
@@ -0,0 +1,36 @@
+import xml.etree.ElementTree as ET
+
+test = """<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc>59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+    </country>
+</data>
+"""
+
+root = ET.fromstring(test)
+
+print(root.tag)
+print(root.attrib)
+for child in root:
+	print(child.tag, child.attrib)
+
+print(root[0][1])
+
+
---
+++ sfbSDNMocker.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+import argparse
+
+logging.basicConfig()
+
+def printHelpText:
+	helpText = """
+		Usage: 
+			-h Prints the help text
+			-s 
+
+	"""
+	print("""
+
+
+
+		""")
