23f35cb9e69e54d178dc0f198c2bd95d4689330d
prickprat <ricky.ratnayake@gmail.com>
2015-09-07 18:12:09 +1000

    fixed sdnlogcleaner merge

--- sdnlogcleaner.py
+++ sdnlogcleaner.py
@@ -5,69 +5,65 @@ import logging_conf
 import argparse
 
 
+START_RX = re.compile(r'Start_Prognosis_datadump >>>>>>>>>>>>>>>>>>: (.*)',
+                      re.DOTALL | re.IGNORECASE)
+END_RX = re.compile(r'<<<<<<<<<<<<<<<<<< Stop_Prognosis_datadump',
+                    re.DOTALL | re.IGNORECASE)
+SPLIT_RX = re.compile(r'IRLYNC\s+httpserv\s+\d+\s+T\d+\s+(.*)',
+                      re.DOTALL | re.IGNORECASE)
+
+
 def main():
     args = parse_sys_args()
-    logCleaner = LogCleaner(args.infile, args.outfile)
-    logCleaner.clean()
+    clean(args.infile, args.outfile)
 
 
-class LogCleaner:
+def clean(in_path, out_path):
+    with open(in_path, mode="rt", errors="strict") as infile:
+        with open(out_path, mode="wt", errors="strict") as outfile:
+            in_sdn_msg = False
+            for line_no, line in enumerate(infile, start=1):
+                in_sdn_msg, cleaned_line = clean_line(line, in_sdn_msg, line_no)
+                outfile.write(cleaned_line)
 
-    def __init__(self, in_path, out_path):
-        self.in_path = in_path
-        self.out_path = out_path
-        self.start_rx = re.compile(r'Start_Prognosis_datadump >>>>>>>>>>>>>>>>>>: (.*)',
-                                   re.DOTALL | re.IGNORECASE)
-        self.end_rx = re.compile(r'<<<<<<<<<<<<<<<<<< Stop_Prognosis_datadump',
-                                 re.DOTALL | re.IGNORECASE)
-        self.split_rx = re.compile(r'IRLYNC\s+httpserv\s+\d+\s+T\d+\s+(.*)',
-                                   re.DOTALL | re.IGNORECASE)
 
-    def clean(self):
-        with open(self.in_path, mode="rt", errors="strict") as infile:
-            with open(self.out_path, mode="wt", errors="strict") as outfile:
-                in_sdn_msg = False
-                for line_no, line in enumerate(infile, start=1):
-                    in_sdn_msg, cleaned_line = self.clean_line(line, in_sdn_msg, line_no)
-                    outfile.write(cleaned_line)
+def clean_line(line, inside_message, line_no=None):
+    """
+    Helper method to parse and clean a IRLYNC Log file (for SDN messages).
+    Automatically right strips for newlines and deals with split-log issues.
 
-    def clean_line(self, line, inside_message, line_no=None):
-        """
-        Helper method to parse and clean a IRLYNC Log file (for SDN messages).
-        Automatically right strips for newlines and deals with split-log issues.
+    Arugments:
+    line            - Input line to clean
+    inside_message  - Current state of parse before cleaning. Boolean
+    line_no         - optional argument for debugging.
 
-        Arugments:
-        line            - Input line to clean
-        inside_message  - Current state of parse before cleaning.
-        line_no         - optional argument for debugging.
+    Returns: tuple (inside_message, cleaned_line)
+    inside_message  - New state of parser (either inside sdn message or not). Boolean
+    cleaned_line    - resulting line from the clean.
 
-        Returns: tuple (inside_message, cleaned_line)
-        inside_message  - New state of parser (either inside sdn message or not).
-        cleaned_line    - resulting line from the clean.
+    """
+    line = line.rstrip('\n')
 
-        """
-        line = line.rstrip('\n')
-
-        if inside_message:
-            match = self.end_rx.search(line)
-            if match:
-                logging.debug("Found 'Stop datadump' marker at line {0}.".format(line_no))
-                return (False, '')
-            # Check for split sdn message dumps
-            match = self.split_rx.search(line)
-            if match:
-                # Should join directly on to previous line with no whitespace
-                logging.debug("Found 'Split logs' marker at line {0}.".format(line_no))
-                return (True, match.group(1))
-            # Include all normal sdn lines with a newline prefix
-            return (True, '\n' + line)
-        else:
-            match = self.start_rx.search(line)
-            if match:
-                logging.debug("Found 'Start datadump' marker at line {0}.".format(line_no))
-                return (True, '\n' + match.group(1))
-            # Ignore non-starting line outside of message
+    if inside_message:
+        match = END_RX.search(line)
+        if match:
+            logging.debug("Found 'Stop datadump' marker at line {0}.".format(line_no))
             return (False, '')
+        # Check for split sdn message dumps
+        match = SPLIT_RX.search(line)
+        if match:
+            # Should join directly on to previous line with no whitespace
+            logging.debug("Found 'Split logs' marker at line {0}.".format(line_no))
+            return (True, match.group(1))
+        # Include all normal sdn lines with a newline prefix
+        return (True, '\n' + line)
+    else:
+        match = START_RX.search(line)
+        if match:
+            logging.debug("Found 'Start datadump' marker at line {0}.".format(line_no))
+            return (True, '\n' + match.group(1))
+        # Ignore non-starting line outside of message
+        return (False, '')
 
 
 def parse_sys_args():
--- unit_test/test_sdnlogcleaner.py
+++ unit_test/test_sdnlogcleaner.py
@@ -1,6 +1,6 @@
 import logging
 import unittest
-from logcleaner import LogCleaner
+import sdnlogcleaner as LC
 
 # Disable non-critical logging for Testing
 logging.disable(logging.CRITICAL)
@@ -19,72 +19,70 @@ XML_1 = """
 class TestLogCleaner(unittest.TestCase):
 
     def test_clean_line_inside(self):
-        lc = LogCleaner("fakepath", "fakepath")
         in_inside_message = True
 
         # Non ending message
         in_line = "Hello."
         expected_line = "\nHello."
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should still be inside the message.")
         self.assertEqual(expected_line, out_line, "Should only prepend with newline.")
 
         # Another non ending message
         in_line = "Hel'<<<<<<<<<<<<<<<<<<lo<Dont><Delete></Delete><Stop></Stop></Dont>."
         expected_line = "\nHel'<<<<<<<<<<<<<<<<<<lo<Dont><Delete></Delete><Stop></Stop></Dont>."
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should still be inside the message.")
         self.assertEqual(expected_line, out_line, "Should only prepend with newline.")
 
         # rightstrip test
         in_line = "\nHello. \n"
         expected_line = "\n\nHello. "
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should still be inside the message.")
         self.assertEqual(expected_line, out_line, "Should prepend newline and right strip newline.")
 
         # End of log message
         in_line = "Hello.<<<<<<<<<<<<<<<<<< Stop_Prognosis_datadump"
         expected_line = ""
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertFalse(out_inside_message, "Should be outside of message.")
         self.assertEqual(expected_line, out_line, "Should be an empty string.")
 
         # Split logs case 1
         in_line = "04/08/2015 09:27:54 IRLYNC   httpserv 000000036 T3800              nMOS>."
         expected_line = "nMOS>."
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should be inside of message.")
         self.assertEqual(expected_line, out_line, "Should be end portion of split with no space.")
 
         # Split logs case 2
         in_line = "04/08/2015 09:27:54 IRLYNC   httpserv 000000036 T3800 "
         expected_line = ""
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should be inside of message.")
         self.assertEqual(expected_line, out_line, "Should be empty string.")
 
     def test_clean_line_outside(self):
-        lc = LogCleaner("fakepath", "fakepath")
         in_inside_message = False
 
         # Non starting message
         in_line = "Hello."
         expected_line = ""
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertFalse(out_inside_message, "Should still be outside the message.")
         self.assertEqual(expected_line, out_line, "Should be an empty string.")
 
         # Found starting message
         in_line = "Start_Prognosis_datadump >>>>>>>>>>>>>>>>>>: Hello."
         expected_line = "\nHello."
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should be inside the message.")
         self.assertEqual(expected_line, out_line, "Should be the end portion of the start.")
 
         # Found starting message
         in_line = "Start_Prognosis_datadump >>>>>>>>>>>>>>>>>>: "
         expected_line = "\n"
-        out_inside_message, out_line = lc.clean_line(in_line, in_inside_message)
+        out_inside_message, out_line = LC.clean_line(in_line, in_inside_message)
         self.assertTrue(out_inside_message, "Should be inside the message.")
         self.assertEqual(expected_line, out_line, "Should be just the newline.")
